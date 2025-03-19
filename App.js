b4e003bb1355086d7f2e20126cc75d9205a51fc1
anask601 <anask60155@gmail.com>
2025-03-19 18:09:48 +0530

    ✨ enhance: implement state management in App component; update Focus component to accept subject input

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState } from "react";
 import {
   Text,
   View,
@@ -7,13 +7,23 @@ import {
   Platform,
   StatusBar,
 } from "react-native";
+import Constants from "expo-constants";
 import { colors } from "./src/utils/colors";
 import { Focus } from "./src/features/Focus";
 
 export default function App() {
+  const [currentSubject, setCurrentSubject] = useState(null);
   return (
     <SafeAreaView style={styles.container}>
-      <Focus />
+      {!currentSubject ? (
+        <Focus addSubject={setCurrentSubject} />
+      ) : (
+        <View>
+          <Text style={{ color: colors.white }}>
+            I am going to render the timer for {currentSubject}
+          </Text>
+        </View>
+      )}
     </SafeAreaView>
   );
 }

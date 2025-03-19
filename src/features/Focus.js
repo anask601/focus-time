efb73b424f8d77c6a86030a785ede8a359003dbd
anask601 <anask60155@gmail.com>
2025-03-19 16:28:32 +0530

    ✨ update Focus component with input field; upgrade react-native-paper to version 5.13.1

--- src/features/Focus.js
+++ src/features/Focus.js
@@ -4,13 +4,19 @@ import { colors } from "../utils/colors";
 
 export const Focus = () => (
   <View style={styles.container}>
-    <Text style={styles.text}>Focus Feature</Text>
+    <View style={styles.inputContainer}>
+      <TextInput label="What would you like to focus on?" />
+    </View>
   </View>
 );
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  inputContainer: {
+    flex: 0.5,
+    padding: 25,
   },
   text: {
     color: colors.white,

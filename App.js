629140ae0fc308485cd01c98746d9fbfd26f74b5
AyeshaKODER <iayeshakhan2004@gmail.com>
2025-03-19 15:42:51 +0530

    ✨ refactor App component: replace View with SafeAreaView for better layout handling

--- App.js
+++ App.js
@@ -1,18 +1,23 @@
 import * as React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import {
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBars,
+} from "react-native";
 import Constants from "expo-constants";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Hello World!</Text>
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 50,
   },
 });
